b63904110f71fc3c83894d28546305705ae9a6e5
alexandrers2307 <82249597+alexandrers2307@users.noreply.github.com>
2021-04-10 09:09:34 -0300

    Initial commit

---
+++ app/src/main/java/com/example/jsonrecyclerview/Feriados.java
@@ -0,0 +1,25 @@
+package com.example.jsonrecyclerview;
+
+public class Feriados {
+
+
+
+    private  final  String name;
+    private  final String date;
+
+    public Feriados(String name, String date){
+
+
+        this.name = name;
+        this.date = date;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+}
+
+
